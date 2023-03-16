368325ea433b41b085174f1ddf4ee27f6ce50579
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-16 06:43:07 -0700

    sizeof()

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d", sizeof(char));
+	printf("Size of an int: %d", sizeof(int));
+	printf("Size of a long int: %d", sizeof(long int));
+	printf("Size of a long long int: %d", sizeof(long long int));
+	printf("Size of a float: %d", sizeof(float));
+	return (0);
+}
