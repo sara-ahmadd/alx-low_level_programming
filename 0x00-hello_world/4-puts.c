03e6cd9ce76fc7f2e03b8ae72f89aeeb4ae53419
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-16 06:26:08 -0700

    first c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
- * main - entry point
- *
- * Return - always 0 (success)
- * */
+/**
+ * main - Entry point of the program.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
