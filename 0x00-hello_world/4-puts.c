0d12eb789b65470866423ff0641b85d3c27cde07
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-16 06:21:46 -0700

    first c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return - always 0 (success)
  * */
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
