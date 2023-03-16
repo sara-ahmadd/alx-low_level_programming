674f7cd2d495dc4e27edf2d6c73d3aa3b43bbab9
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-16 07:01:11 -0700

    intel syntax

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	sprintf(command,"echo \"and that piece of art is useful\" - Dora Korpar, 2015-10-19" >&2 "");
+	return (0);
+}
