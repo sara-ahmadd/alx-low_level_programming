9c036a5aa9c73b34df3959edfbef2b0dd7118aee
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-17 12:06:33 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i = '0';
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
