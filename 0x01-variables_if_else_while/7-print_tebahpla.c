a75244b8bee512a08d073ba97c4659acbfb4e9ed
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-17 13:04:15 -0700

    m

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,15 +7,16 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
-	char letters[] ="abcdefghijklmnopqrstuvwxyz";
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 	int length = strlen(letters);
 
-        for (i = length - 1; i >= 0 ; i--)
-        {
-         putchar(letters[i]);
+	for (i = length - 1; i >= 0 ; i--)
+	{
+	putchar(letters[i]);
 	}
 	putchar('\n');
 	return (0);
