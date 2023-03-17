d32da42b3e550f2f45342c8aa6ff5aef5edffaf6
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-17 12:42:20 -0700

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * reverse - reverse the input string
+ *
+ * Return: reversed string
+ */
+const char* reverse(char str[])
+{
+	int i,j;
+	char temp;
+	int len = strlen(str);
+
+	for (i = len - 1 ,j = 0 ;j < (len/2) ; j-- , i++)
+	{
+		temp = str[j];
+		str[j] = str[i];
+		str[i] = temp;
+	}
+	return str;
+}
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letters[] ="abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int length = strlen(letters);
+	reverse(letters);
+
+	for (i = 0 ; i < length ; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
