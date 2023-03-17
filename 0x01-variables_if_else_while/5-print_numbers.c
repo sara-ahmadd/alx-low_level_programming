3a50410fb8c07a95dd63e0e6da08d316b3e79e3c
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-17 11:57:08 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0' ; i < '10' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
