6a7b8483ecc50f9b40c8984e8f2281a6804439d5
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 13:54:26 -0700

    get the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,5 @@ int print_last_digit(int n)
 {
 	int last = abs(n) % 10;
 	
-	if (n < 1 && n > 0)
-	{
-		int last = abs(n * 10) % 10;
-
-		_putchar('0' + last);
-		return (last);
-	}
-	_putchar('0' + last);
 	return (last);
 }
