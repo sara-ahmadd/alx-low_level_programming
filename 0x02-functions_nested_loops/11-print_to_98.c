7cd4feaa4a99403f2b636540f26b1176c745989b
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 16:13:13 -0700

    print natural num till 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,20 +19,26 @@ void print_to_98(int n)
 			{
 				printf("%d\n", n);
 			}
-			printf("%d, ", n);
+			else
+			{
+				printf("%d, ", n);
+			}
 			n--;
 		}
 		
 	}
 	if (n < 98)
 	{
-		while (n < 98)
+		while (n <= 98)
 		{
 			if (n == 98)
 			{
 				printf("%d\n", n);
 			}
-			printf("%d, ", n);
+			else
+			{
+				printf("%d, ", n);
+			}
 			n++;
 		}
 	}
