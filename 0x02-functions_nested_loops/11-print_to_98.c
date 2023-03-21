16c3ccae618d32a652c4f3ccf1da284d23bfb7b5
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 15:37:28 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,25 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (int i = n; n >= 98; i--)
+		int i = n;
+
+		while (n >= 98)
 		{
 			_putchar('0' + i);
 			_putchar(' ');
+			i--;
 		}
 		_putchar('\n');
 	}
 	if (n < 98)
 	{
-		for (int i = n; n <= 98; i++)
+		int i = n;
+
+		while (n <= 98)
 		{
 			_putchar('0' + i);
 			_putchar(' ');
+			i++;
 		}
 		_putchar('\n');
 	}
