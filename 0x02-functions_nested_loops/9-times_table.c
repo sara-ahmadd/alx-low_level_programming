7f712cd14f4253fddbc76bab77c3ecddf41463e4
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 14:41:17 -0700

    the times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * void times_table(void);
+*/
+void times_table(void)
+{
+	int i, j;
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		
+		while (j < 10)
+		{
+			int result = i * j;
+			_putchar(result);
+			_putchar(',');
+			_putchar('\t');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
