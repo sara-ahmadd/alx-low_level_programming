fcbc1b145c107af54a235cd726b077608e5c57d4
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 11:39:28 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <ctype.h>
 
+
+void print_alphabet(char alpha[], int size)
+{
+	for(int i = 0; i < size; i++)
+        {
+                int letter = tolower(alpha[i]);
+                _putchar(letter);
+        }
+}
+
 /**
  * main - entery point
  *
@@ -10,13 +20,10 @@
 int main(void)
 {
 	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int size_alpha = sizeof(alpha);
-
-	for(int i = 0; i < size_alpha; i++)
-	{	
-		int letter = tolower(alpha[i]);
-		_putchar(letter);
-	}
+	int size_alpha = sizeof(alpha) / sizeof(alpha[0]);
+	
+	print_alphabet(alpha, size_alpha);
+	
 	_putchar('\n');
 	return (0);
 }
