3651a076f44dfd8b9bfeb5ad5c3b08aaeb1fb4b4
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 14:50:10 -0700

    the times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 			_putchar('0' + (result / 10));
 			_putchar('0' + (result % 10));
 			_putchar(',');
-			_putchar('\t');
+			_putchar(' ');
 			j++;
 		}
 		_putchar('\n');
