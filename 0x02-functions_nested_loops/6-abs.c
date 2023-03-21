434b5b5bc83fe0ae938cf86359aad8ae4113a6f7
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 13:17:49 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs -return the absolute value of a number
+ * @n: the number
+ * Return: the computed value
+*/
+int _abs(int n)
+{
+	return(abs(n));
+}
