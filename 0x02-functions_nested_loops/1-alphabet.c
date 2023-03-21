2206ad17563d108f3b0ab9436d887bd10b00e16f
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 12:23:15 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+ * print_alphabet - print letters in lower case
+ */
 void print_alphabet(void)
 {
 	char letters[] = "abcdefghijklmnopqrstuvwxyz";
 	int size = strlen(letters);
 	int i = 0;
 
-	while(i < size)
+	while (i < size)
 	{
 		_putchar(letters[i]);
 		i++;
