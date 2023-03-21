369e74440a4d1abee8e9ee0c685d74e6d0975816
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 14:53:55 -0700

    the times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * void times_table(void);
+ * times_table - print times table
 */
 void times_table(void)
 {
 	int i, j;
 	i = 0;
+
 	while (i < 10)
 	{
 		j = 0;
-		
+
 		while (j < 10)
 		{
 			int result = (i * j);
 			_putchar('0' + (result / 10));
-			_putchar('0' + (result % 10));
 			_putchar(',');
 			_putchar(' ');
 			j++;
