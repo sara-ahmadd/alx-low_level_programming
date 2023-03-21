c8d8c3c81499daf4308eda2b9179815fb2c2bbbc
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 12:42:01 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,23 +4,24 @@
 
 /**
  * print_alphabet_x10 - print alphabet 10 times in lower case
-  */
+ */
 
 void print_alphabet_x10(void)
 {
-	int i = 0; 
+	int i = 0;
+
 	while (i < 10)
 	{
 		char letters[] = "abcdefghijklmnopqrstuvwxyz";
 		int length = strlen(letters);
-		int j = 0;		
+		int j = 0;
 
-       		 while (j < length)
-       		 {
-               		 _putchar(letters[j]);
-        	        j++;
-	       	 }
-	        _putchar('\n');
+		while (j < length)
+		{
+			_putchar(letters[j]);
+			j++;
+		}
+		_putchar('\n');
 		i++;
 	}
 }
