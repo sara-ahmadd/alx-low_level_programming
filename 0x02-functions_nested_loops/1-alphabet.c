4d3b42a936578cb5fd03309d3b17c5ddf28b3226
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-21 11:32:15 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype>
+
+/**
+ * main - entery point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int size_alpha = sizeof(alpha);
+
+	for(int i = 0; i < size_alpha; i++)
+	{	
+		int letter = tolower(alpha[i]);
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0);
+}
