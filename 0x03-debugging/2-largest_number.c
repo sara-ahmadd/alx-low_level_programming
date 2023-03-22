42371dcd4068b73a388ac57776b2cc9f7eed290a
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-22 13:16:16 -0700

    largest num

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,6 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	printf("%d is the largest number", largest);
-	return (largest);
+	/*printf("%d is the largest number\n", largest);*/
+	return ("%d is the largest number\n", largest);
 }
