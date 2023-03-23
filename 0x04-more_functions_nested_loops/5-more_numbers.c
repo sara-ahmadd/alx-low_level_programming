799447c7d260e27a039c841cfbeae241561e6498
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 14:20:51 -0700

    0 - 14 printed ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,21 +5,13 @@
  */
 void more_numbers(void)
 {
-	int i , j;
+	int i, j;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
-			else
-			{
-				_putchar(j + '0');
-			}
+			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
