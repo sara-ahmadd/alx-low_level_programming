3fa7061c042df7c9b0f844130509e1d6a61767b8
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 14:55:53 -0700

    0 - 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,6 +11,7 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+
 			if (j < 10)
 			{
 				_putchar(j + '0');
@@ -21,6 +22,6 @@ void more_numbers(void)
 				_putchar((j % 10) + '0');
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
