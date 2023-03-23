d8afdce857f0cf29d849008d76d9d7d7b87bd2c9
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 16:21:40 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entery point
+ * Return: 1 if succeed
+ */
+int main(void)
+{
+	int i;
+
+	for (i 0 =; i < 100; i++)
+	{
+		if (i % 3)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 && i % 5)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+}
