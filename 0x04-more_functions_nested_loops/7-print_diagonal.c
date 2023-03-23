c5f0bbe5ed934791d09611bd9458801d6327b1e4
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 15:15:05 -0700

    draw diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * void print_diagonal(int n)
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i = 0;
+
+		while (i < n)
+		{
+			_putchar(' ');
+			_putchar('\');
+			_putchar('\n');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
