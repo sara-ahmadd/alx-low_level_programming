436660f7bfee56f4bba188906b5e1774d2733a6a
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 15:40:21 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,21 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * void print_diagonal(int n)
+ * print_diagonal - print diagonal
+ * @n: number of backspaces
  */
 void print_diagonal(int n)
 {
 	if (n > 0)
 	{
-		int i = 0;
+		int i = 0, j = 0;
 
 		while (i < n)
 		{
-			_putchar(' ');
+			while (j < n)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
 			i++;
 		}
-		_putchar('\');
-		_putchar('\n');
+			_putchar('\n');
 	}
 	else
 	{
