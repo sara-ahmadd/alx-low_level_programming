f84254a199b8a2345b62ff6b2aeab1394bc7b672
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 16:25:15 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,15 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		if (i % 3)
+		if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (i % 3 && i % 5)
+		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
