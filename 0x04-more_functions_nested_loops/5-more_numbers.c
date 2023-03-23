1d87dfa63e1504203c1a98775177a4e8f428b8f0
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 07:01:15 -0700

    print nums 0 - 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,13 @@ void more_numbers(void)
 			{
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
+				j++;
 			}
-			_putchar(j + '0');
-			j++;
+			else
+			{
+				_putchar(j + '0');
+				j++;
+			}
 		}
 		_putchar('\n');
 		i++;
