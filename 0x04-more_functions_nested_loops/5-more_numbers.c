0aacbff330c2a755087ba55e8598c886a72e8a83
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-23 06:49:58 -0700

    print nums 0 - 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print num from 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int i = 0, j = 0;
+
+	while (i <= 10)
+	{
+		while (j < 15)
+		{
+			_putchar(i + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
