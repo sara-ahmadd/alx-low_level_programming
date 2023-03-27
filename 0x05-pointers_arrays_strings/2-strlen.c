63ed4ea622c9b830304be9326d2ede63c56878a7
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-27 14:35:04 -0700

    return length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - return length of an input string
+ * @s: the input string
+ * Return: the length of that string
+ */
+
+int _strlen(char *s)
+{
+	return  (strlen(*s));
+}
