dc3db03dfa9ce5356037a16afd1ca631b386050c
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-27 15:01:24 -0700

    revers string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - reverse an input string
+ * @s: the input string
+ */
+
+void rev_string(char *s)
+{
+	strrev(s);
+}
