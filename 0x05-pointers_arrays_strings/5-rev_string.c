c59bab4c3f2a218855b3ce8063ba963cb7a0fcf5
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-27 15:19:31 -0700

    revers string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,12 @@ void rev_string(char *s)
 {
 	int len = strlen(s);
 	int i;
-	char str[];
+	char temp;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; i < len / 2; i--)
 	{
-		strcpy(str, s[i]);
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
 	}
-	strcpy(s, str);
 }
