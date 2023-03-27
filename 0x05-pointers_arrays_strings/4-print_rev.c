c1259064a0e146e7057375cc5b879957ab7a3526
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-27 14:47:13 -0700

    print a stirng in reversed pattern

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - print the string in reversed pattern
+ * @s: the input string
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+}
