2af85cf2d104c5ef4439c46d7ba3d2a4448e682e
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-30 16:17:15 -0700

    strcmp() in c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compair between 2 strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: the number of different characters if there are diff chars
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
