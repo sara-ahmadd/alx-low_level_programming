5c4926a68a97d05d8925690bc35a480ccabdf2d3
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-30 16:01:47 -0700

    concat 2 strings again

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat - concatenate 2 strings
+ * @dest: first string
+ * @src: the second string
+ * @n: the maximum number of chars in the second string
+ * Return: concatenated 2 strings
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	dest = strlen(dest, src[n]);
+	return (dest);
+}
