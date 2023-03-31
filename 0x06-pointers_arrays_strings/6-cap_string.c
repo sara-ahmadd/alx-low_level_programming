bb5697291f237e0819d64cb1785e55d125d69520
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-30 18:12:48 -0700

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * *cap_string - capitalize a string
+ * @s: the string
+ * Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+			s[i] = toupper(s[i]);
+
+		if (s[i] == ' ')
+		{
+			s[i + 1] = toupper(s[i + 1];
+		}
+	}
+	return (s);
+}
