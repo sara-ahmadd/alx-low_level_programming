7253746d28492b496325c3c9a34625c8fabf87f6
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-30 17:07:30 -0700

    to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <ctype>
+/**
+ * *string_toupper - change chars to uppercase
+ * @str: the string
+ * Return: upper case string
+ */
+char *string_toupper(char *str)
+{
+	return (toupper(str));
+}
