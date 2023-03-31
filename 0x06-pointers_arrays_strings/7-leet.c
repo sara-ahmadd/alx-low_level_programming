d43bc357b5ff2a0b13e363d19b5ba35a4ed8401b
Sara Ahmad <saraahmad2060039@gmail.com>
2023-03-30 18:38:15 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,29 +6,19 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char leet[20] = "OL?EA??T";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+	
+		for (j = 0; leet[j] != '\0'; j++)
 		{
-			s[i] = 4 + '0';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = 3 + '0';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = 0 + '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = 7 + '0';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = 1 + '0';
+			if (s[i] == leet[j] ||
+				s[i] - 32 == leet[j])
+			{
+				s[i] = j + '0';
+			}
 		}
 	}
 
