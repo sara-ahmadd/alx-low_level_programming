01efbee79b13609c009d4520aa206318e867bf79
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-03 06:43:24 -0700

    memcpy() in c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copy a string to another string
+ * @dest: the array to which another string copied
+ * @src: the source string
+ * @n: the amount of bytes to be copied
+ * Return: pointer to the destination array
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
