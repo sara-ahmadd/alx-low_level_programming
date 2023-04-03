6ce862b850080acd98b8575bd0b1f060a06c9368
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-03 06:16:45 -0700

    memset() in c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  * *_memset - fill a block of memory with a specific value
@@ -12,6 +11,6 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char **mem = void *memset(s, b, n);
-	return (*mem);
+	char *mem = memset(s, b, n);
+	return (mem);
 }
