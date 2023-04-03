cd1a9650041f16af77ef19e88e0df748facf5b66
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-03 06:57:00 -0700

    strchr() in c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strchr - searches for a char in a string
+ * @s: the string to searh in
+ * @c: the char to search for
+ * Return : a pointer to the found char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int len = strlen(s) + 1;
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s[i]);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
