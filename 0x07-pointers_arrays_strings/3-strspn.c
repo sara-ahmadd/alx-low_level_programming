5dd7634751e59d65110887480027229e57d6a8c2
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-03 07:59:34 -0700

    strspn() in c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,18 +12,15 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, k = 0;
 
-	for (i = 0; i < strlen(s); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < strlen(accept); j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				k++;
 			}
-			else
-			{
-				continue;
-			}
+
 		}
 	}
 	return (k);
