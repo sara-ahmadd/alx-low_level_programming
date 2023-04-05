13f35cedab64fa6088327b0ccb12571fb0144886
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-05 16:38:27 -0700

    recursive_sqrt() in c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,13 +5,19 @@
  * @n: the number input
  * Return: square root of that number
  */
+/**
+ * square - check if the (value) is square root of (num)
+ * @num: the number to check its square root
+ * @value: the square root
+ * Return: the square root or -1 if no square root
+ */
 int square(int num, int value)
 {
 	if (value * value == num)
 	{
 		return (value);
 	}
-	else if(value * value < num)
+	else if (value * value < num)
 	{
 		return (square(num, value + 1));
 	}
