902bf0e097895a2ce81451b4f98beddc1dbdfc7d
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-05 15:50:05 -0700

    recursive_pow() in c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - print x to the power of y
+ * @x: the main number
+ * @y: the power
+ * Return: x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
