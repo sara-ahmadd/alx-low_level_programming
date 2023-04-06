e2ebcd8106cee78e56e764f4952c911854e2404e
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-05 17:40:28 -0700

    is_prime_recursion() in c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * is_prime_recursion - return if the number is prime or not
+ * is_prime_number - return if the number is prime or not
  * @n: the number
  * Return: o if not prime or 1 if prime
  */
@@ -9,14 +9,13 @@ int is_prime_number(int n)
 {
 	if (n > 0)
 	{
-		return (n / is_prime_number(n - 1));
-	}
-	else if (n == 1)
-	{
-		return (n);
-	}
-	else
-	{
-		return (0);
+		if (n % is_prime_number(n - 1) == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 	}
 }
