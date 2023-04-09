af0e26332fc3c42af1022c682126e73182714562
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-09 05:24:13 -0700

    print arguments count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entery point to the program
+ * @argc: the arguments count
+ * @argv: array of the arguments passed to the function
+ * Return: 0 (success) or 1 (failure)
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
