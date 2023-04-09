a39e29894775106429e7b5ed213c5dfef8dc9b12
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-09 05:19:23 -0700

    arguments to main()

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,12 +9,12 @@ int main(int argc, char **argv)
 {
 	if (argc == 1)
 	{
-		printf("%s", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	else
 	{
-		argv[0] == argv[1];
-		printf("%s", argv[0]);
+		argv[0] = argv[1];
+		printf("./%s\n", argv[0]);
 	}
 	return (0);
 }
