98dbabf438030616bf50d21f9b772bee1ac3e202
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-09 14:55:38 -0700

    number of coins to give a change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,34 +11,28 @@
 
 int main(int argc, char **argv)
 {
-	int i, numberOfCoins, result = atoi(argv[1]);
+	int i, numberOfCoins = 0, value = atoi(argv[1]);
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc < 2 || argc > 2)
+	if (argc == 2)
 	{
-		return (printf("Error\n"), 1);
-	}
-	else if (result < 0)
-	{
-		return (printf("%d\n", 0), 0);
-	}
-	else
-	{
-		for (i = 0; i < sizeof(coins) / sizeof(int); i++)
+		for (i = 0; i < 5; i++)
 		{
-			if (result > coins[i])
+			if (value >= coins[i])
 			{
-				result -= coins[i];
-				if (result > 0)
-				{
-					numberOfCoins++;
-				}
-				else
+				numberOfCoins += value / coins[i];
+				value = value % coins[i];
+				if (value % coins[i] == 0)
 				{
 					break;
 				}
+
 			}
 		}
 		return (printf("%d\n", numberOfCoins), 0);
+	}
+	else
+	{
+		return (printf("Error\n"), 1);
 	}
 }
