3c8b0a34f3c267a4763a9fdcfdbbecce405fbcf5
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-09 05:38:27 -0700

    arguments count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - entery point to the program
  * @argc: the arguments count
  * @argv: array of the arguments passed to the function
  * Return: 0 (success) or 1 (failure)
  */
+
 int main(int argc, _attribute_((unused))char **argv)
-{	
+{
 	printf("%d\n", argc - 1);
 	return (0);
 }
