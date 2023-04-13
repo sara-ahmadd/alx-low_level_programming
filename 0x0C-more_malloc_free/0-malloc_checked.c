75bac258e82d25606e199115e493ae0afe745ce7
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-13 14:30:34 -0700

    malloc_checked()

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: the size to be allocated
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *memo;
+
+	memo = malloc(b);
+	if (memo == NULL)
+	{
+		exit(98);
+		return (1);
+	}
+	else
+	{
+		return (memo);
+	}
+}
