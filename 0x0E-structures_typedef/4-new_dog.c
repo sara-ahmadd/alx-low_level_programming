39fa492adf4be5237297221207fc37a1fadec563
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-17 15:18:28 -0700

    create instance from struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - create new dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: dog info
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
+	n_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (n_dog == NULL)
+	{
+	       return (NULL);
+      	}
+	n_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (n_dog->name == NULL)
+	{
+		free (n_dog);
+		return (NULL);
+	}
+	n_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (n_dog->owner == NULL)
+	{
+		free (n_dog->name);
+		free (n_dog);
+		return (NULL);
+	}
+	strcpy(n_dog->name, name);
+	strcpy(n_dog->owner, owner);
+	n_dog->age = age;
+	return (n_dog);
+
+}
