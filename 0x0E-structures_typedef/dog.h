f09f328f604f15a5ec3fec1332dd7343821fed66
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-17 13:36:47 -0700

    structs in c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Decription: a struct describe dog info
+ */
 struct dog
 	{
 		char *name;
