1953ae45407992e80f6edeeb689f82aeab55ab29
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-17 15:26:05 -0700

    struct instance

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,26 +13,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
+
 	if (name == NULL || age < 0 || owner == NULL)
 	{
 		return (NULL);
 	}
-	n_dog = (dog_t *)malloc(sizeof(dog_t));
+	n_dog = (dog_t *) malloc(sizeof(dog_t));
 	if (n_dog == NULL)
 	{
-	       return (NULL);
-      	}
+		return (NULL);
+	}
 	n_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (n_dog->name == NULL)
 	{
-		free (n_dog);
+		free(n_dog);
 		return (NULL);
 	}
 	n_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (n_dog->owner == NULL)
 	{
-		free (n_dog->name);
-		free (n_dog);
+		free(n_dog->name);
+		free(n_dog);
 		return (NULL);
 	}
 	strcpy(n_dog->name, name);
