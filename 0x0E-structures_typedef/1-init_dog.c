91f90c389b03a9e79b486da9839e9e305c441149
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-17 13:58:54 -0700

    function creates a astruct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 /**
  * init_dog - initialize avarible of type struct dog
- * @dog: the struct name
+ * @d: the struct name
  * @name: the dog name
  * @age: the dog age
  * @owner: the owner of the dog
  * Return: void
  */
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
