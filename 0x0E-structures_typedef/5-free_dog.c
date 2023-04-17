da38b5204677fead23aa4267d3eb7a355baf8178
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-17 15:33:03 -0700

    free struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,7 +8,16 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
 }
