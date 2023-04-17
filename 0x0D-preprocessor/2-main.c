1dcd9a81f7487f3e4aefd8cb8462d343dddec0b5
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-17 16:12:56 -0700

    print compiled file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entery point to the program
+ * Return: (0) success oe non-zero value (failure)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
