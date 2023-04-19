0fcda7ac623965fbb98d1efc1a53e9f2a13d035b
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-19 16:23:55 -0700

    calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #ifndef CALC
 #define CALC
+#include <stdlib.h>
 /**
  * struct op - Struct op
  *
@@ -9,8 +9,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,27 +6,29 @@
  * @argv: arguments array
  * Return: 0 (success) or non-zero value (failure)
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	int (*op_f)(int, int), num1, num2;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	num1 = atoi(argv[1]);	
+
+	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
 	op_f = get_op_func(argv[2]);
 	if (!op_f)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	printf("%d\n", op_f(num1, num2));
 	return (0);
