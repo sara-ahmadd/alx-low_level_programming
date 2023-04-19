474441ded07c88ffb8316b2439b739830b47c9bf
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-19 13:36:03 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ * @name: the name of the person
+ * @f: pointer to a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	while (*name)
+	{
+		_putchar(*name);
+	}
+	_putchar('\n');
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNC_POINTERS
+#define FUNC_POINTERS
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
