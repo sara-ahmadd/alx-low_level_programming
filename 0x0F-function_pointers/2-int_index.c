014e01fcb0885464133114e0ece0052043a208d3
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-19 14:26:55 -0700

    int_index()

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - searches for integer
+ * @array: array to search inside
+ * @size: size of the array
+ * @cmp: pointer to the function that searches
+ * Return: index to the matched integer or -1 if no matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && size > 0 && cmp)
+	{
+		int i, result;
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) >  0)
+			{
+				return (i);
+			}
+		}
+	
+	}
+
+	return (-1);
+
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #include <stddef.h>
+#include <stdlib.h>
 #ifndef FUNC_POINTERS
 #define FUNC_POINTERS
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
