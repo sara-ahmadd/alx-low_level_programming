1581c5d369ecad1a7c231dfe63639a4395fde2c6
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-20 13:33:45 -0700

    add_all()

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,10 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+	unsigned int sum = 0;
 	unsigned int i;
 	va_list ap;
 	
-	if (n == 0)
-	{
-		return (0);
-	}
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
