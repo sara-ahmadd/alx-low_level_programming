11ad53d265904beb6f6340d5c95e2708b6b8d38b
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-20 12:29:29 -0700

    var_func print all types

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * format_integer - prints integer
+ * @separator: separate arguments
+ * @args: list of arguments
+ */
+void format_integer( char *separator, va_list args)
+{
+	printf("%s%d", separator, va_arg(args, int));
+}
+
+/**
+ * format_charac - prints character
+ * @separator: separate arguments
+ * @args: list of arguments
+ */
+void format_charac(char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
+}
+/**
+ * format_float - print float
+ * @separator: separates arguments
+ * @args: list of arguments
+ */
+void format_float(char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
+}
+/**
+ * format_string - print string
+ * @separator: separates argumnets
+ * @args: list of arguments
+ */
+void format_string(char *separator, va_list args)
+{
+	char *str = va_arg(args, str);
+	printf("%s%S", separator, str ? str : "(nil)");
+}
+/**
+ * print_all - print args of any datatype
+ * @format: list of types of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	arguments_format args_format[] = {
+		{"c", format_charac},
+		{"i", format_integer},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+	int i = 0, j = 0;
+	va_list args;
+	char *sep = ", ";
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		while (args_format[j].arg_type)
+		{
+			if (format[i] == args_format[j].arg_type[0])
+			{
+				args_format[j].f(sep, args);
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VAR_FUNC
 #define VAR_FUNC
 #include <stdarg.h>
+
+/**
+ * struct arguments - arguments
+ * @arg_type: type of argument
+ * @f: function for specified datatype
+ */
+typedef struct arguments {
+	char *arg_type;
+	void (*f)(char *, va_list);
+}arguments_format;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
