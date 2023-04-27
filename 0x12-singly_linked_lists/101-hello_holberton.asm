ee63e0e70f50e267263fc28a93b6180221a44014
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-27 13:30:04 -0700

    asm file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,10 @@
-global _start
+section		.text
+	extern printf
+	global main
+main:
+	mov		edi, msg
+	mov		eax, 0
+	call	printf
+section		.data
+	msg db 'Hello, Holberton', 0xa, 0
 
-section .text
-
-_start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, Holberton\n",
-  mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
-  syscall           ; );
-
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, Holberton", 10
-  msglen: equ $ - msg
