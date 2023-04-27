b1c7b581fdf36dc783f7a63744d0d0fc61e90fc9
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-27 12:49:58 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,18 +8,36 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode = malloc(sizeof(list_t));
+	list_t *current = *head;
 
-	if (*head == NULL)
+	if (!head || !newNode)
+	{
+		return (NULL);
+	}
+
+	if (str)
+	{
+		newNode->str = strdup(str);
+		if (!newNode->str)
+		{
+			free(newNode);
+			return (NULL);
+		}
+		newNode->len = strlen(str);
+	}
+
+
+	if (current)
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+	else
 	{
 		*head = newNode;
 	}
-
-	list_t *current = *head;
-
-	while (current != NULL)
-	{
-		current = current->next;
-	}
-	current->next = newNode;
 	return (newNode);
 }
