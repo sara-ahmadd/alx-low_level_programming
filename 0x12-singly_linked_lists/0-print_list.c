aaa5c3e3bc2bfaa19e515245bc9c1bb8b69f6dea
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-27 08:30:18 -0700

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 int _strlen(char *str)
 {
 	int i = 0;
-	
+
 	if (str == NULL)
 	{
 		return (0);
@@ -27,12 +27,12 @@ int _strlen(char *str)
 size_t print_list(const list_t *h)
 {
 	const list_t *current = h;
-	
 	unsigned int counter = 0;
 
 	while (current != NULL)
 	{
-		printf("[%d]%s\n", _strlen(current->str),  current->str == NULL ? "(nil)" : current->str);
+		printf("[%d]%s\n", _strlen(current->str),
+				current->str == NULL ? "(nil)" : current->str);
 		current = current->next;
 		counter++;
 	}
