6f09227e2296c17e5033b572d194548bf0f57dd0
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-29 05:13:06 -0700

    return length of linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * listint_len - return length of linked list
+ * @h: head node of the lists
+ * Return: number of elements in the linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *current = malloc(sizeof(listint_t));
+	size_t count = 0;
+
+	if (h)
+	{
+		current = h;
+
+		while (current)
+		{
+			count++;
+			current = current->next;
+		}
+	}
+	return (count);
+}
