bc084459322b62b4a41282115af9e30d4da6fe85
Sara Ahmad <saraahmad2060039@gmail.com>
2023-04-29 06:40:03 -0700

    a

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - delete last item in the list
+ * @head: the head node
+ *
+ * Return: n in the head node
+ */
+int pop_listint(listint_t **head)
+{
+	if (!head)
+		return (0);
+
+	listint_t *curr = *head;
+
+	while (curr->next)
+	{
+		curr = curr->next;
+	}
+	curr = NULL;
+	return (head->n);
+}
