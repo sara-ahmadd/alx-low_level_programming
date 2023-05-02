cdf4bd886a5e9fedeae92289c118f9762cb32f47
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 01:26:55 -0700

    get node at i

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return node at specific index
+ * @head: the head node
+ * @index: the required index
+ * Return: the node at (index)
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *currNode;
+	unsigned int i;
+
+	for (currNode = head, i = 0; currNode && i < index;
+		       currNode = currnode->next, i++)
+	{
+		;
+	}
+	return (currNode);
+}
