d65691a46c8a8456c32723dce8fd14196af63146
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 01:48:50 -0700

    create node at index i

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,9 +1,30 @@
 #include "lists.h"
 
 /**
+ * insert_nodeint_at_index - create node at index i
+ * @head: the head node
+ * @idx: the required index
+ * @n: the value of n in the new node
+ * Return: the new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *current;
+	unsigned int i;
 
+	for (current = *head, i = 0; current && i < idx - 1; current = current->next, i++)
+	{
+		listint_t newNode = malloc(sizeof(listint_t));
+
+		if (!newNode)
+		{
+			return (NULL);
+		}
+		newNode->n = n;
+		newNode->next = NULL;
+
+		newNode = current->next;
+		return (newNode);
+	}
 }
