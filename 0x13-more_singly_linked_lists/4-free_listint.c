d7191d5dc38e39676890b2ff4da73100e31c9e4a
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 00:16:35 -0700

    free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,12 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * free_listint - free the linked list
  * @head: the head node of the list
+ *
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
