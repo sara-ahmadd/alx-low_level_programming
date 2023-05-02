ba446bdfed3e319c13e19291ecd92d0003f22f5c
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 14:06:01 -0700

    a

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -16,7 +16,7 @@ listint_t *find_listint_loop(listint_t *head)
 		return (NULL);
 	}
 	curr = head;
-	nxt = head->next;
+	nxt = head;
 
 	while (curr)
 	{
@@ -26,6 +26,7 @@ listint_t *find_listint_loop(listint_t *head)
 		}
 
 		curr = curr->next;
+		nxt = nxt->next->next;
 	}
 	return (NULL);
 }
