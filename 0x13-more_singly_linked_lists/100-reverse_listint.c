158ad8196ac07c2b4cc3366bcebf90dfb01bc37d
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 12:46:46 -0700

    revers list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse linked list
+ * @head: the head node
+ * Return: the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *curr, *prev,  *next;
+
+	if (!head || !*head)
+	{
+		return (NULL);
+	}
+
+	prev = NULL;
+	curr = *head;
+
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*head = prev;
+	return (*head);
+}
