c5458932ebb0717c2fefbc2de406dd795053ed44
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 01:36:43 -0700

    sum of n inside nodes

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint add values inside nodes
+ * @head: the head node
+ * Return: the sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current;
+
+	current = head;
+
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+
+}
