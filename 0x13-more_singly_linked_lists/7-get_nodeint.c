698d1ba320d2d472d1fdbc016ee082e021097c63
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 01:29:15 -0700

    get node at i

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	for (currNode = head, i = 0; currNode && i < index;
-		       currNode = currnode->next, i++)
+		       currNode = currNode->next, i++)
 	{
 		;
 	}
