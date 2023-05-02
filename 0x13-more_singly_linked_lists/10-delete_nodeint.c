0d1b59ff70efbcb26189e23721437bc76288675e
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 02:16:05 -0700

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node at index
+ * @head: head node
+ * @index: the req index
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t curr;
+	unsigned int i;
+
+	if (!head)
+	{
+		return (-1);
+	}
+
+	curr = *head;
+
+	while (curr)
+	{
+		if (i == index - 1)
+		{
+			free(curr->next);
+		}
+		i++;
+		curr = curr->next;
+	}
+	return (1);
+}
