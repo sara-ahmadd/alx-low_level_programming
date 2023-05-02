fc3c1c742852b0e9c570a5b752234bfe5b859826
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-02 13:44:27 -0700

    free list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,8 +1,26 @@
 #include "lists.h"
 
 /**
+ * free_listint_safe - free linled list
+ * @h: root node
+ * Return: number of nodes
  */
 
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *curr;
+	size_t i = 0;
+
+	if (!h)
+		return;
+
+	while (curr)
+	{
+		listint_t tmp;
+		tmp = curr;
+		curr = curr->next;
+		free(tmp);
+		i++;
+	}
+	return (i)
 }
