2e2021957f8288ff6c05b366a7f3cd05fb62be48
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-08 09:47:20 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, n;
+	int file, n, len = 0;
 
 	if (!filename)
 	{
@@ -19,7 +19,12 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
-	n = write(file, text_content ,strlen(text_content));
+
+	while (text_content && text_content[len])
+	{
+		len++;
+	}
+	n = write(file, text_content, len);
 
 	if (n == -1)
 	{
