cdd2a874ff39ecaa7cd02fd08f30242aa881f907
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-08 09:52:48 -0700

    b

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,22 +11,38 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, n;
 	ssize_t result;
-	char buff[1024];
+	char *buff;
 
+	if (!filename || !letters)
+	{
+		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 
-	if (fd == -1 || !filename || !letters)
+	if (fd == -1)
+		return (0);
+	
+	buff = malloc(sizeof(char) * letters + 1);
+	if (!buff)
 		return (0);
 
 	result = read(fd, buff, letters);
 
 	if (result == -1)
+	{
+		free(buff);
 		return (0);
+	}
+
+	buff[letters] = '\0';
 
 	n = write(STDOUT_FILENO, buff, result);
 	if (n == -1)
+	{
+		free(buff);
 		return (0);
-
+	}
+	free(buff);
 	close(fd);
 
 	return (result);
