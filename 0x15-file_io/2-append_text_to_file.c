28acc6c181b982f4c4009dd54ba96b78fafe3cce
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-08 10:03:52 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,11 +13,16 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, n, len = 0;
 
-	file = open(filename, O_EXCL | O_APPEND);
+	file = open(filename, O_WRONLY |  O_APPEND);
 
 	if (file == -1)
 	{
 		return (-1);
+	}
+
+	if (!text_content)
+	{
+		return (1);
 	}
 
 	while (text_content && text_content[len])
