49b75c26078a496ae995967f2990f8f864fd97d9
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-08 09:43:25 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file = open(filename, O_WRONLY | O_CREAT, 0644);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	n = write(file, text_content ? text_content : "", strlen(text_content));
 
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * append_text_to_file - add text to a file
+ * @filename: the name of the file
+ * @text_content: content to be written to the file
+ *
+ * Return: 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+}
