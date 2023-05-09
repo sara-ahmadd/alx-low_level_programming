97cbdfbf2ff09b247449eef6d8543ad0938ba6cb
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-09 09:44:44 -0700

    fixing errors

--- Eshell/main.c
+++ Eshell/main.c
@@ -10,58 +10,78 @@
 
 int main(int argc, char **argv)
 {
-	char *prompt = "(Eshell) :)", *lineptr;
-	ssize_t n, chars_read, tokens_count = 0, i;
+	char *prompt = "(Myshell) :)", *lineptr = NULL;
+	ssize_t n, chars_read, tokens_count = 0, i, j;
 	char *line_cp = NULL, *token;	
 
 	const char *delims;
 	
-	delims = " \n";
+	delims = " ";
 	n = 0;
 	
-	(void)argc;
-	(void)argv;
-
 	while (1)
 	{
-		printf("%s ", prompt);
-		chars_read = getline(&lineptr, &n, stdin);
-		if (chars_read == -1)
-		{
-			printf("Exit eshell\n");
-			return (-1);
-		}
-		line_cp = malloc(sizeof(char) * chars_read);
-		if (line_cp == NULL)
-		{
-			perror("can't allocate memory");
-			return (-1);
-		}
-		strcpy(line_cp, lineptr);
-		/*calc number of tokens*/
-		token = strtok(lineptr, delims);
-		while (token != NULL)
-		{
-			tokens_count++;
-			token = strtok(NULL, delims);
-		}
+	printf("%s ", prompt);
+	chars_read = getline(&lineptr, &n, stdin);
+	if (chars_read == -1)
+	{
+		printf("Exit Myshell\n");
+		free(lineptr);
+		return (-1);
+	}
+	line_cp = malloc(sizeof(char) * chars_read);
+	if (line_cp == NULL)
+	{
+		perror("can't allocate memory");
+		free(lineptr);
+		return (-1);
+	}
+	strcpy(line_cp, lineptr);
+	/*calc number of tokens*/
+	token = strtok(line_cp, delims);
+	while (token != NULL)
+	{
 		tokens_count++;
-		
-		argv = malloc(sizeof(token) * tokens_count);
-		token = strtok(line_cp, delims);
+		token = strtok(NULL, delims);
+	}
+		tokens_count++;
+	
+	argv = malloc(sizeof(char *) * tokens_count);
+	if (argv == NULL)
+	{
+		perror("Error in memory allocation");
+		free(line_cp);
+		free(lineptr);
+		return (-1);
+	}
+	token = strtok(line_cp, delims);
 		for (i = 0; token != NULL; i++)
 		{
 			argv[i] = malloc(sizeof(char) * strlen(token));
+			if (argv[i] == NULL)
+			{
+				perror("Error in memory allocation");
+				for (j = 0; j < i; j++)
+				{
+					free(argv[j]);
+				}
+				free(argv);
+				free(line_cp);
+				free(lineptr);
+				return (-1);
+			}
 			strcpy(argv[i], token);
 			token = strtok(NULL, delims);
 		}
-		argv[i] = '\0';
 
-
-		printf("%s\n", lineptr);
+		for(i = 0; i < tokens_count; i++)
+		{
+			argv[i] != NULL && printf("%s\n", argv[i]);
+			free(argv[i]);
+		}
 		free(argv);
 		free(line_cp);
-		free(lineptr);
 	}
+	free(lineptr);	
 	return (0);
 }
